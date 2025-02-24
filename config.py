9621a7e2664550a4f1d2147d45cf22aa5ef63d04
KarimUPF <karim.hamed01@estudiant.upf.edu>
2025-02-24 16:48:32 +0100

    URI fix

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:  
     SECRET_KEY = os.getenv('SECRET_KEY', 'TEST_SECRET_KEY')  
-    #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://ptm:Popojad123!@localhost/ptm_nexus'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://karimhmd:popojad123@localhost/ptm_nexus'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://ptm:Popojad123!@localhost/ptm_nexus'
+    #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://karimhmd:popojad123@localhost/ptm_nexus'
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False

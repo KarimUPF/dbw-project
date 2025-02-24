535fe5f3079d0b8b4b523280511689543fc525b9
Maider <maiderupvehu@gmail.com>
2025-02-24 17:56:59 +0100

    Changed main html to index.html and added signup_form and login_form

--- routes/main.py
+++ routes/main.py
@@ -1,9 +1,13 @@
 from flask import Blueprint, render_template
 from flask_login import login_required
+from forms.login_form import LoginForm
+from forms.registration_form import RegistrationForm
 
 main = Blueprint('main', __name__)
 
 @main.route('/')
 def home():
-    return render_template('compare.html', title="Home")
+    login_form = LoginForm()
+    signup_form = RegistrationForm()
+    return render_template('index.html', title="Home", login_form=login_form, signup_form=signup_form)
 
